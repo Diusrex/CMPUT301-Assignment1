fef9a01514f898ee341079c4bd107a1874b782fb
Diusrex <killakan002@hotmail.com>
2015-02-01 15:34:49 -0700

    Set up the TravelApplication.
    It will hold onto the only TravelClaimOwner

---
+++ src/com/example/assignment1/TravelApplication.java
@@ -0,0 +1,24 @@
+package com.example.assignment1;
+
+import android.app.Application;
+import android.content.Context;
+
+public class TravelApplication extends Application {
+    transient private static TravelClaimOwner owner = null;
+    private static Context context;
+    
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        TravelApplication.context = getApplicationContext();
+    }
+
+    public static TravelClaimOwner getMainOwner() {
+        if (owner == null) {
+            SaverAndLoader saverLoader = new AndroidSaver(context);
+            owner = new TravelClaimOwner(saverLoader);
+        }
+
+        return owner;
+    }
+}
