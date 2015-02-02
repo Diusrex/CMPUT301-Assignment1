8b8541eda16cd8fcf5e94e127df0186709c91794
Diusrex <killakan002@hotmail.com>
2015-02-02 11:34:29 -0700

    Shifted some more functions to utilities

--- src/com/example/assignment1/Utilities.java
+++ src/com/example/assignment1/Utilities.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 import android.content.Context;
 import android.util.Pair;
@@ -13,16 +14,19 @@ public final class Utilities {
     private static final float epsilon = 0.001f;
 
     private Utilities() {
-        
+
     }
-    
-    // Origional list will be changed
-    public static ArrayList<Pair<String, Float>> mergeAllDuplicatedPayments(ArrayList<Pair<String, Float>> allPayments) {
+
+    public static boolean calLessThan(Calendar n1, Calendar n2) {
+        return n1.getTimeInMillis() < n2.getTimeInMillis();
+    }
+
+    // Original list will be changed
+    public static List<Pair<String, Float>> mergeAllDuplicatedPayments(List<Pair<String, Float>> allPayments) {
         sortList(allPayments);
 
-
         String previousItem = null;
-        ArrayList<Pair<String, Float>> mergedPayments = new ArrayList<Pair<String,Float>>();
+        List<Pair<String, Float>> mergedPayments = new ArrayList<Pair<String, Float>>();
         int pos = -1;
         for (Pair<String, Float> item : allPayments) {
             if (item.first.equals(previousItem)) {
@@ -34,7 +38,7 @@ public final class Utilities {
                 ++pos;
             }
         }
-        
+
         return mergedPayments;
     }
 
@@ -42,7 +46,7 @@ public final class Utilities {
         return new Pair<String, Float>(item1.first, item1.second + item2.second);
     }
 
-    private static void sortList(ArrayList<Pair<String, Float>> allPayments) {
+    private static void sortList(List<Pair<String, Float>> allPayments) {
         // Will sort them based on the strings. This way, can scroll through
         // very easily
         Collections.sort(allPayments, new Comparator<Pair<String, Float>>() {
@@ -52,12 +56,12 @@ public final class Utilities {
             }
         });
     }
-    
+
     public static String getFormattedDateString(Context context, int stringId, Calendar date) {
         String dateOutput = DateFormat.getDateInstance().format(date.getTime());
         return context.getResources().getString(stringId, dateOutput);
     }
-    
+
     public static boolean floatsAreDifferent(float f1, float f2) {
         return (Math.abs(f1 - f2) > epsilon);
     }
