5ddf1fee43b623b6f4b22e208cb7bef4c6cfcb5b
Diusrex <killakan002@hotmail.com>
2015-02-01 20:25:29 -0700

    Set up the expense class with all its needed members

--- src/com/example/assignment1/TravelExpense.java
+++ src/com/example/assignment1/TravelExpense.java
@@ -1,5 +1,46 @@
 package com.example.assignment1;
 
+import java.util.Calendar;
+import java.util.Currency;
+import java.util.GregorianCalendar;
+
 public class TravelExpense {
+    private Calendar date;
+    private String description;
+    private Category category;
+    private float amount;
+    private Currency currency;
+
+    enum Category {
+        AIR_FAIR, GROUND_TRANSPORT, VEHICLE_RENTAL, FUEL, PARKING, REGISTARTION, ACCOMMODATION, MEAL, OTHER
+    };
+
+    TravelExpense() {
+        date = new GregorianCalendar();
+        description = "";
+        category = Category.OTHER;
+        amount = 0;
+        currency = CurrencyHandler.getLocalDefaultCurrency();
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
 
 }
